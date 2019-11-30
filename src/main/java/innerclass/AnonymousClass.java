a957b7a44489f7f6aca6de255cfe381809148f59
sharibislam <sharib.islam@yahoo.com>
2019-11-30 19:30:57 +0530

    update inner class
    just added one line to show the git change functionality

--- src/main/java/innerclass/AnonymousClass.java
+++ src/main/java/innerclass/AnonymousClass.java
@@ -5,6 +5,7 @@ public class AnonymousClass {
     class inner {
         public void show() {
             System.out.println("Inner class");
+              System.out.println("Changes from git");
         }
     }
 }
