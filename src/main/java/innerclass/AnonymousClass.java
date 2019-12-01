17ed330bc50ad636d4f44c7825e578e3e35cb570
sharibislam <sharib.islam@yahoo.com>
2019-12-01 23:42:25 +0530

    updating data in FB

--- src/main/java/innerclass/AnonymousClass.java
+++ src/main/java/innerclass/AnonymousClass.java
@@ -6,6 +6,7 @@ public class AnonymousClass {
         public void show() {
             System.out.println("Inner class");
               System.out.println("Changes from git");
+            System.out.println("getting from feature branch");
         }
     }
 }
